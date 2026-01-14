3263d928e5a01d91563298aae3acaf6607452b22
Robert Hawker <robertthedev@gmail.com>
2026-01-14 11:46:38 +0000

    feat(home-page): created the app home-page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,38 @@
+import { LucideChevronRight } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { decks } from '../data/decks';
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>Home Page</h1>
+    <main className="bg-neutral-50 p-4 sm:p-10">
+      <h1 className="text-2xl font-bold">Flashcard Decks</h1>
+      <div className="mt-4 grid grid-cols-1 overflow-hidden rounded-lg sm:mt-10 sm:grid-cols-4 sm:gap-10">
+        {decks.map(deck => {
+          return (
+            <Link href="/" key={deck.id}>
+              <div className="flex flex-row items-start justify-between border-b border-b-neutral-300 bg-white py-4 sm:flex-col sm:rounded-lg sm:border-2 sm:border-black sm:p-2 sm:p-6">
+                <div className="flex flex-row items-start gap-8">
+                  <div className="relative h-12 w-12 sm:h-24 sm:w-24">
+                    <Image fill alt="" src={deck.icon_svg} />
+                  </div>
+
+                  <div className="flex flex-col">
+                    <p className="text-base font-bold sm:mt-2 sm:text-xl">
+                      {deck.name}
+                    </p>
+                    <p className="text-sm sm:mt-2">{deck.word_count} words</p>
+                  </div>
+                </div>
+                <div className="sm:hidden">
+                  <LucideChevronRight />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </main>
   );
 }
