b9a9ce6e9ce70ff50785fc315e0c8e7be4794efd
Robert Hawker <robertthedev@gmail.com>
2026-01-23 22:51:09 +0000

    feat(feature): added eight new decks

--- app/data/decks.ts
+++ app/data/decks.ts
@@ -1,27 +1,51 @@
+import { airport } from './words/airport';
 import { animals } from './words/animals';
+import { artStudio } from './words/art-studio';
 import { bathroom } from './words/bathroom';
+import { bugsAndInsects } from './words/bugs-and-insects';
 import { clothes } from './words/clothes';
 import { familyMembers } from './words/family-members';
+import { fastFood } from './words/fast-food';
 import { fruitAndVegetables } from './words/fruit-and-vegetables';
+import { globalWarming } from './words/global-warming';
 import { humanBody } from './words/human-body';
-import { humanitarianHelp } from './words/humanitarian-help';
 import { kitchen } from './words/kitchen';
+import { landscapes } from './words/landscapes';
+import { military } from './words/military';
 import { musicalInstruments } from './words/musical-instruments';
+import { naturalDisaster } from './words/natural-disaster';
+import { office } from './words/office';
+import { pets } from './words/pets';
+import { police } from './words/police';
 import { seaLife } from './words/sea-life';
 import { space } from './words/space';
+import { sport } from './words/sport';
 import { stationery } from './words/stationery';
+import { summerCamp } from './words/summer-camp';
 
 export const decks = [
+  airport,
   animals,
   bathroom,
+  bugsAndInsects,
   clothes,
+  artStudio,
   familyMembers,
+  fastFood,
   fruitAndVegetables,
+  globalWarming,
   humanBody,
-  humanitarianHelp,
   kitchen,
+  landscapes,
+  military,
   musicalInstruments,
+  naturalDisaster,
+  office,
+  pets,
+  police,
   seaLife,
   space,
+  sport,
   stationery,
+  summerCamp,
 ];
