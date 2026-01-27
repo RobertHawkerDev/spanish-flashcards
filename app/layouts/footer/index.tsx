828b52dd3931452b2a192bb20c577f8d591f8e95
Robert Hawker <robertthedev@gmail.com>
2026-01-27 05:08:41 +0000

    feat(layout): added social links to footer

--- app/layouts/footer/index.tsx
+++ app/layouts/footer/index.tsx
@@ -1,28 +1,37 @@
-import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa6';
+import clsx from 'clsx';
 
 import companyName from '@/app/utils/company-name';
 
+import socialLinks from './social-links';
+
 export default function Footer() {
+  const footerClasses = clsx(
+    'hidden md:flex min-h-16 w-full',
+    'border-t border-neutral-50 bg-white',
+    'dark:border-neutral-800 dark:bg-neutral-950/70 backdrop-blur',
+    'pt-12 pb-[calc(env(safe-area-inset-bottom)+4rem)]',
+    'lg:pt-6 lg:pb-8',
+  );
+
   return (
-    <footer className="flex min-h-16 items-center justify-between border-t border-t-neutral-300 bg-white px-10 py-5">
-      <div>
-        <p className="text-sm">
+    <footer className={footerClasses}>
+      <div className="container-x mx-auto flex w-full max-w-7xl flex-col items-center justify-between gap-10 lg:flex-row lg:gap-6">
+        <div className="flex flex-row items-center gap-5">
+          {socialLinks.map(socialLink => (
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              key={socialLink.url}
+              href={socialLink.url}
+              aria-label={socialLink.name}
+            >
+              {socialLink.icon}
+            </a>
+          ))}
+        </div>
+        <p className="text-sm font-medium text-neutral-700 dark:text-neutral-400">
           © {new Date().getFullYear()} {companyName}
         </p>
-      </div>
-      <div className="flex flex-row items-center gap-5">
-        <a target="_blank" href="https://www.facebook.com/spanishflashcardsapp">
-          <FaFacebook size={21} />
-        </a>
-        <a
-          target="_blank"
-          href="https://www.instagram.com/spanishflashcardsapp"
-        >
-          <FaInstagram size={21} />
-        </a>
-        <a target="_blank" href="https://www.tiktok.com/@spanishflashcards">
-          <FaTiktok size={21} />
-        </a>
       </div>
     </footer>
   );
---
+++ app/layouts/footer/social-links.tsx
@@ -0,0 +1,21 @@
+import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa6';
+
+const socialLinks = [
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/spanishflashcardsapp',
+    icon: <FaFacebook size={22} />,
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/spanishflashcardsapp',
+    icon: <FaInstagram size={22} />,
+  },
+  {
+    name: 'TikTok',
+    url: 'https://www.tiktok.com/@spanishflashcards',
+    icon: <FaTiktok size={22} />,
+  },
+];
+
+export default socialLinks;
