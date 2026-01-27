a94a5c16a868c0d9cef9e4845fe59d3e97c5ec8e
Robert Hawker <robertthedev@gmail.com>
2026-01-27 10:33:15 +0000

    refactor(other): removed default layout from not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,6 @@ import { LucideHome } from 'lucide-react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 
-import DefaultLayout from './layouts/default-layout';
 import companyName from './utils/company-name';
 
 export const metadata: Metadata = {
@@ -12,22 +11,20 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <DefaultLayout>
-      <main className="flex flex-col items-center px-5 py-10 md:py-12">
-        <h1 className="text-center text-3xl font-bold sm:text-5xl">
-          404 - Not Found
-        </h1>
-        <p className="mt-5 text-center sm:mt-7">
-          The page you are looking for does not exist or has been moved.
-        </p>
-        <Link
-          className="mt-7 flex items-center justify-center rounded-full bg-black px-8 py-4 font-semibold text-white hover:bg-neutral-800 sm:mt-9"
-          href="/"
-        >
-          <LucideHome size={20} />
-          <p className="ml-2">Home</p>
-        </Link>
-      </main>
-    </DefaultLayout>
+    <main className="flex flex-col items-center px-5 py-10 md:py-12">
+      <h1 className="text-center text-3xl font-bold sm:text-5xl">
+        404 - Not Found
+      </h1>
+      <p className="mt-5 text-center sm:mt-7">
+        The page you are looking for does not exist or has been moved.
+      </p>
+      <Link
+        className="mt-7 flex items-center justify-center rounded-full bg-black px-8 py-4 font-semibold text-white hover:bg-neutral-800 sm:mt-9"
+        href="/"
+      >
+        <LucideHome size={20} />
+        <p className="ml-2">Home</p>
+      </Link>
+    </main>
   );
 }
